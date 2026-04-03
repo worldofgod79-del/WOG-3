e8b1997bf7f14407de058b08bdc00a62342d65f2
worldofgod79-del <worldofgod79@gmail.com>
2026-04-03 20:52:25 +0530

    Create lib/core/theme/app_colors.dart

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkViolet = Color(0xFF1A1A2E); 
+  static const Color liteYellow = Color(0xFFFFF9C4);
+  static const Color gray = Color(0xFFBDBDBD);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color surface = Color(0xFF16213E); 
+}
